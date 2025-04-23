977a92187e105dc18f0e91d26e9986708409bb5a
unknown <misamoskalenko318@gmail.com>
2025-04-23 17:47:26 +0200

    added fetching articles and view type selector

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,106 +1,52 @@
-import { Article, ArticleList, ArticleView } from 'entities/Article';
-import { memo } from 'react';
+import { ArticleList, ArticleView, ArticleViewSelector } from 'entities/Article';
+import { getArticlesPageError, getArticlesPageIsLoading, getArticlesPageView } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
+import { fetchArticlesList } from 'pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList';
+import { articlesPageActions, articlesPageReducer, getArticles } from 'pages/ArticlesPage/model/slice/articlesPageSlice';
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useInitialEffetct } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
     className?: string;
 }
 
-const article = {
-    id: '1',
-    title: 'Javascript news',
-    subtitle: "What's new in JS for 2025?",
-    img: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
-    views: 1022,
-    createdAt: '16.04.2025',
-    user: {
-        id: '1',
-        username: 'admin',
-        avatar: 'https://i.ibb.co/B52J0x4K/image.png',
-    },
-    type: [
-        'IT',
-    ],
-    blocks: [
-        {
-            id: '1',
-            type: 'TEXT',
-            title: 'The title of this block',
-            paragraphs: [
-                "The program that is traditionally called 'Hello, world!' is very simple. It outputs the phrase 'Hello, world!' somewhere, or something similar, using some language.",
-                'JavaScript is a language that can be run in different environments. In our case, we are talking about browsers and the Node.js server platform. If you have not written a single line of JS code yet and are reading this text in a browser, on a desktop computer, this means that you are literally seconds away from your first JavaScript program.',
-                "There are other ways to run JS code in a browser. So, if we talk about the usual use of JavaScript programs, they are loaded into the browser to ensure the operation of web pages. As a rule, the code is formatted as separate files with the .js extension, which are included in web pages, but the program code can also be included directly in the page code. All this is done using the <script> tag. When the browser detects such code, it executes it. You can find more details about the script tag on w3school.com. In particular, let's look at an example that demonstrates working with a web page using JavaScript, provided on this resource. This example can also be run using this resource (look for the Try it Yourself button), but we will do it a little differently. Namely, we will create a new file in some text editor (for example, in VS Code or Notepad++), which we will call hello.html, and add the following code to it:",
-            ],
-        },
-        {
-            id: '4',
-            type: 'CODE',
-            code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
-        },
-        {
-            id: '5',
-            type: 'TEXT',
-            title: 'The title of this block',
-            paragraphs: [
-                "The program that is traditionally called 'Hello, world!' is very simple. It outputs the phrase 'Hello, world!' somewhere, or something similar, using some language.",
-                "There are other ways to run JS code in a browser. So, if we talk about the usual use of JavaScript programs, they are loaded into the browser to ensure the operation of web pages. As a rule, the code is formatted as separate files with the .js extension, which are connected to web pages, but the program code can also be included directly in the page code. All this is done using the <script> tag. When the browser detects such code, it executes it. Details about the script tag can be found on the w3school.com website. In particular, let's look at an example demonstrating working with a web page using JavaScript, given on this resource. This example can also be run using this resource (look for the Try it Yourself button), but we will do it a little differently. Namely, we will create a text editor (for example, in VS Code or in Notepad++) a new file, which we will call hello.html, and add the following code to it:",
-            ],
-        },
-        {
-            id: '2',
-            type: 'IMAGE',
-            src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
-            title: 'Figure 1 - screenshot of the site',
-        },
-        {
-            id: '3',
-            type: 'CODE',
-            code: "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);",
-        },
-        {
-            id: '7',
-            type: 'TEXT',
-            title: 'The title of this block',
-            paragraphs: [
-                "JavaScript is a language that can be run in different environments. In our case, we are talking about browsers and the Node.js server platform. If you haven't written a single line of JS code yet and you are reading this text in a browser, on a desktop computer, it means that you are literally seconds away from your first JavaScript program.",
-                "There are other ways to run JS code in a browser. So, if we talk about the usual use of JavaScript programs, they are loaded into the browser to ensure the operation of web pages. As a rule, the code is formatted as separate files with the .js extension, which are included in web pages, but the program code can also be included directly in the page code. All this is done using the <script> tag. When the browser detects such code, it executes it. Details about the script tag can be found on the w3school.com website. In particular, let's look at an example demonstrating the work with a web page using JavaScript, given on this resource. This example can be launched using this resource (look for the Try it Yourself button), but we will do it a little differently. Namely, we will create a new file in some text editor (for example, in VS Code or Notepad++), which we will call hello.html, and add the following code to it:",
-            ],
-        },
-        {
-            id: '8',
-            type: 'IMAGE',
-            src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
-            title: 'Figure 1 - screenshot of the site',
-        },
-        {
-            id: '9',
-            type: 'TEXT',
-            title: 'The title of this block',
-            paragraphs: [
-                "JavaScript is a language that can be run in different environments. In our case, we are talking about browsers and the Node.js server platform. If you haven't written a single line of JS code yet and you are reading this text in a browser, on a desktop computer, it means that you are literally seconds away from your first JavaScript program.",
-            ],
-        },
-    ],
-} as Article;
+const reducers: ReducersList = {
+    articlesPage: articlesPageReducer,
+};
 
 const ArticlesPage = ({ className }: ArticlesPageProps) => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const articles = useSelector(getArticles.selectAll);
+    const isLoading = useSelector(getArticlesPageIsLoading);
+    const view = useSelector(getArticlesPageView);
+    const error = useSelector(getArticlesPageError);
+
+    useInitialEffetct(() => {
+        dispatch(fetchArticlesList());
+        dispatch(articlesPageActions.initState());
+    });
+
+    const onChangeView = useCallback((view: ArticleView) => {
+        dispatch(articlesPageActions.setView(view));
+    }, [dispatch]);
+
     return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            <ArticleList
-                view={ArticleView.BIG}
-                articles={
-                    new Array(16)
-                        .fill(0)
-                        .map((item, index) => ({
-                            ...article,
-                            id: String(index),
-                        }))
-                }
-            />
-        </div>
+        <DynamicModuleLoader reducers={reducers}>
+            <div className={classNames(cls.ArticlesPage, {}, [className])}>
+                <ArticleViewSelector view={view} onViewClick={onChangeView} />
+                <ArticleList
+                    isLoading={isLoading}
+                    view={view}
+                    articles={articles}
+                />
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
