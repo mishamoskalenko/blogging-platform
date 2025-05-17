297c0d07c76ca7eac1ae22c99695078ee4b3d545
unknown <misamoskalenko318@gmail.com>
2025-05-17 16:45:39 +0200

    fixed screenshot test

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
 import MainPage from './MainPage';
 
 export default {
@@ -15,7 +16,12 @@ const Template: ComponentStory<typeof MainPage> = () => <MainPage />;
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({
+
+})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+
+})];
